a419d0b202f529815c8e43683d158bc5929bb18b
eltempez <tempez.el@gmail.com>
2025-01-07 11:51:00 +0100

    Upload example outputs for changes from the old program

---
+++ example_outputs/rsrz/update/R_files_tmp/params.R
@@ -0,0 +1,4 @@
+pathdata = './saconf_out/PDB'
+pathsaconf = './saconf_out'
+pathFig = './saconf_out/graph'
+file1 = './saconf_out/pdb_list_encode.id'
---
+++ example_outputs/unresolved/update/R_files_tmp/params.R
@@ -0,0 +1,4 @@
+pathdata = 'outputs/unresolved/update/PDB'
+pathsaconf = 'outputs/unresolved/update/'
+pathFig = 'outputs/unresolved/update/graph'
+file1 = 'outputs/unresolved/update/pdb_list_encode.id'
